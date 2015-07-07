952e2f3374912f666a0054a7ee1762adf7861215
audrie <audrie.talbot@gmail.com>
2015-07-07 16:52:39 -0700

    All functioning except for sort.

---
+++ store.js
@@ -0,0 +1,35 @@
+var posts = [
+  {
+    id: '1',
+    title: 'I love blood',
+    body: 'asldkjhfalskdjfbaksjdbfjsabdlkjfhasjkdakskljasdfksjahfdjkkjhsakjkjsad.'
+  },
+  {
+    id: '2',
+    title: 'Thievery',
+    body: 'asldkfjalksdjflkasjvklasndvkljaskldjflkasfkllsakdlasjd.'
+  },
+  {
+    id: '3',
+    title: 'Count',
+    body: 'alskdjflkasdfjlkasdnflkajdflksklfjalksfd.'
+  }
+];
+
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'I am so sorry. How are you feeling now?'
+  },
+  {
+    id: '2',
+    postId: '1',
+    text: 'laksdfas;dlkf.'
+  },
+  {
+    id: '3',
+    postId: '2',
+    text: 'a;lskdfjaslkdfj.'
+  }
+];
